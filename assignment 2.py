86f7c133ccf24ef7231ef8b99891a89345e5cfa2
Greatestguru007 <126727956+Greatestguru007@users.noreply.github.com>
2023-03-09 00:14:41 +0100

    Python assignment 2

---
+++ assignment 2.py
@@ -0,0 +1,8 @@
+num1 = float(input("Enter a number: "))
+num2 = float(input("Enter another number: "))
+sum = num1 + num2 
+print(sum)
+if sum > 100:
+    print("The sum is large.")
+if sum <= 100:
+    print("The sum is too small.")
